cbc4a7c56fb624e9b7eb8b2be88505a816d9744b
unboundspirit3-sudo <unboundspirit3@gmail.com>
2026-03-24 21:52:59 +0000

    Delete SRC/main.jsx

--- SRC/main.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import GoWithYourFlow from './GoWithYourFlow.jsx'
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <GoWithYourFlow />
-  </React.StrictMode>
-)
